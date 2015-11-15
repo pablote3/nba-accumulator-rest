13a6a0a6626a96b822cbc0d367f1d5a4666091b4
Paul Rossotti <rossotti.paul@gmail.com>
2015-11-15 08:45:30 -0800

    introduce IllegalArgumentException to findTeamByKey

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/app/resources/TeamResource.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/app/resources/TeamResource.java
@@ -1,5 +1,6 @@
 package com.rossotti.basketball.app.resources;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -12,6 +13,8 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,7 +28,7 @@ public class TeamResource {
 
 	@Autowired
 	private TeamDAO teamDAO;
-	
+
 	@GET
 	@Path("/{key}")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -35,6 +38,24 @@ public class TeamResource {
 		return Response.ok(pubTeam)
 			.link(uriInfo.getAbsolutePath(), "team")
 			.build();
+	}
+
+	@GET
+	@Path("/{key}/{asOfDate}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response findTeamByKey(@Context UriInfo uriInfo, @PathParam("key") String key, @PathParam("asOfDate") String asOfDateString) {
+		
+		try {
+			DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+			LocalDate asOfDate = formatter.parseLocalDate(asOfDateString);
+			Team team = teamDAO.findTeamByKey(key, asOfDate);
+			PubTeam pubTeam = team.toPubTeam(uriInfo);
+			return Response.ok(pubTeam)
+				.link(uriInfo.getAbsolutePath(), "team")
+				.build();
+		} catch (IllegalArgumentException e) {
+			throw new BadRequestException("asOfDate must be yyyy-MM-dd format", e);
+		}
 	}
 
 	@POST
