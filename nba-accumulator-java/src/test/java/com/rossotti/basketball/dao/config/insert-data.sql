7aa38dee080dbd0264513df418b88d9f9d7eb93b
Paul Rossotti <rossotti.paul@gmail.com>
2015-11-21 16:20:58 -0800

    update test cases

--- nba-accumulator-java/src/test/java/com/rossotti/basketball/dao/config/insert-data.sql
+++ nba-accumulator-java/src/test/java/com/rossotti/basketball/dao/config/insert-data.sql
@@ -1,2 +1,2 @@
-insert into team VALUES(1, 'HAR', 'Harlem', 'East', 'Atlantic', 'Harlem', '1926-10-15', 'Harlem Globetrotters', 'harlem-globetrotters', 'Globetrotters', 'Rucker Park', 'NY', '2012-12-31');
+insert into team VALUES(1, 'HAR', 'Harlem', 'East', 'Atlantic', 'Harlem', '2009-07-01', 'Harlem Globetrotters', 'harlem-globetrotters', 'Globetrotters', 'Rucker Park', 'NY', '2010-06-30');
 select count(*) from team;
