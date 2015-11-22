e639ed2ed2ba16c656b26d4dffabf143761c0ad7
Paul Rossotti <rossotti.paul@gmail.com>
2015-11-21 16:15:51 -0800

    rename findTeamByAsOfDate

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/app/resources/TeamResource.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/app/resources/TeamResource.java
@@ -33,7 +33,7 @@ public class TeamResource {
 	@Path("/{key}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response findTeamByKey(@Context UriInfo uriInfo, @PathParam("key") String key) {
-		Team team = teamDAO.findTeamByKey(key, new LocalDate());
+		Team team = teamDAO.findTeamByKeyAsOfDate(key, new LocalDate());
 		PubTeam pubTeam = team.toPubTeam(uriInfo, key, null);
 		return Response.ok(pubTeam)
 			.link(uriInfo.getAbsolutePath(), "team")
@@ -48,7 +48,7 @@ public class TeamResource {
 		try {
 			DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
 			LocalDate asOfDate = formatter.parseLocalDate(asOfDateString);
-			Team team = teamDAO.findTeamByKey(key, asOfDate);
+			Team team = teamDAO.findTeamByKeyAsOfDate(key, asOfDate);
 			PubTeam pubTeam = team.toPubTeam(uriInfo, key, asOfDateString);
 			return Response.ok(pubTeam)
 				.link(uriInfo.getAbsolutePath(), "team")
--- nba-accumulator-java/src/main/java/com/rossotti/basketball/dao/TeamDAO.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/dao/TeamDAO.java
@@ -7,7 +7,7 @@ import org.joda.time.LocalDate;
 import com.rossotti.basketball.models.Team;
 
 public interface TeamDAO {
-	public Team findTeamByKey(String key, LocalDate asOfDate);
+	public Team findTeamByKeyAsOfDate(String key, LocalDate asOfDate);
 	public List<Team> findTeamsByDateRange(LocalDate fromDate, LocalDate toDate);
 	public void createTeam(Team team);
 }
