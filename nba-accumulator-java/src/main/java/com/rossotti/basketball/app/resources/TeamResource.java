1289c5e1a5d7696ad338669bbfa982c3f7e900e4
Paul Rossotti <rossotti.paul@gmail.com>
2015-11-24 05:33:01 -0800

    update POST to return PubTeam and handle exceptions

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/app/resources/TeamResource.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/app/resources/TeamResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,6 +13,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import org.hibernate.PropertyValueException;
 import org.joda.time.LocalDate;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
@@ -19,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rossotti.basketball.dao.TeamDAO;
+import com.rossotti.basketball.dao.exceptions.DuplicateEntityException;
 import com.rossotti.basketball.models.Team;
 import com.rossotti.basketball.pub.PubTeam;
 
@@ -44,7 +47,6 @@ public class TeamResource {
 	@Path("/{key}/{asOfDate}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response findTeamByKey(@Context UriInfo uriInfo, @PathParam("key") String key, @PathParam("asOfDate") String asOfDateString) {
-		
 		try {
 			DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
 			LocalDate asOfDate = formatter.parseLocalDate(asOfDateString);
@@ -60,10 +62,26 @@ public class TeamResource {
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response createTeam(Team team) {
-		teamDAO.createTeam(team);
-		String result = "Team created : " + team.toString();
-		return Response.ok(result).build();
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response createTeam(@Context UriInfo uriInfo, Team team) {
+		try {
+			teamDAO.createTeam(team);
+			PubTeam pubTeam = team.toPubTeam(uriInfo, team.getKey(), null);
+			return Response.ok(pubTeam)
+				.link(uriInfo.getAbsolutePath(), "team")
+				.build();
+		} catch (DuplicateEntityException e) {
+			throw new BadRequestException("team " + team.getKey() + " already exists", e);
+		} catch (PropertyValueException e) {
+			throw new BadRequestException("missing required field(s)", e);
+		}
 	}
-	
+
+//	@PUT
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	public Response updateTeam(Team team) {
+//		teamDAO.updateTeam(team);
+//		String result = "Team updated : " + team.toString();
+//		return Response.ok(result).build();
+//	}
 }
