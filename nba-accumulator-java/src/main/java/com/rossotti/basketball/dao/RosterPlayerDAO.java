cd6bc70bf22bde1bca44d7534c7895cb49271164
Paul Rossotti <rossotti.paul@gmail.com>
2016-01-07 17:00:29 -0800

    correct spelling of birthdate

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/dao/RosterPlayerDAO.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/dao/RosterPlayerDAO.java
@@ -7,11 +7,11 @@ import org.joda.time.LocalDate;
 import com.rossotti.basketball.model.RosterPlayer;
 
 public interface RosterPlayerDAO {
-	public RosterPlayer findRosterPlayer(String lastName, String firstName, LocalDate birthDate, LocalDate asOfDate);
+	public RosterPlayer findRosterPlayer(String lastName, String firstName, LocalDate birthdate, LocalDate asOfDate);
 	public RosterPlayer findRosterPlayer(String lastName, String firstName, String teamKey, LocalDate asOfDate);
-	public List<RosterPlayer> findRosterPlayers(String lastName, String firstName, LocalDate birthDate);
+	public List<RosterPlayer> findRosterPlayers(String lastName, String firstName, LocalDate birthdate);
 	public List<RosterPlayer> findRosterPlayers(String teamKey, LocalDate asOfDate);
 	public RosterPlayer createRosterPlayer(RosterPlayer rosterPlayer);
 	public RosterPlayer updateRosterPlayer(RosterPlayer rosterPlayer);
-	public RosterPlayer deleteRosterPlayer(String lastName, String firstName, LocalDate birthDate, LocalDate asOfDate);
+	public RosterPlayer deleteRosterPlayer(String lastName, String firstName, LocalDate birthdate, LocalDate asOfDate);
 }
