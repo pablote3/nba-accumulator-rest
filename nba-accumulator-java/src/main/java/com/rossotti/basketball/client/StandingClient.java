9cc230783e33b53a8c8b6a354d0a22aa598b52c3
Paul Rossotti <rossotti.paul@gmail.com>
2016-01-17 08:12:31 -0800

    added provider for Jackson object mapper

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/client/StandingClient.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/client/StandingClient.java
@@ -1,28 +1,26 @@
 package com.rossotti.basketball.client;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rossotti.basketball.app.provider.JsonProvider;
 import com.rossotti.basketball.client.dto.Standings;
 
 public class StandingClient {
+	
+	private static ObjectMapper mapper = JsonProvider.buildObjectMapper();
 
 	public static void main(String[] args) {
 
-		final String authHeader = "Bearer " + "accessToken";
 
-		String urlStanding = "https://erikberg.com/nba/standings/";
 		String event = "20130131.json";
 //		String fileStanding = "/home/pablote/pdrive/pwork/play/accumulator/config/fileStanding/2015_16";
 //		Source source = Source.API;
@@ -31,27 +29,29 @@ public class StandingClient {
 		client.register(new ClientRequestFilter() {
 			@Override
 			public void filter(ClientRequestContext requestContext) throws IOException {
-				requestContext.getHeaders().add(HttpHeaders.AUTHORIZATION, authHeader);
-				requestContext.getHeaders().add(HttpHeaders.USER_AGENT, "userAgentName");
+//				requestContext.getHeaders().add(HttpHeaders.AUTHORIZATION, authHeader);
+//				requestContext.getHeaders().add(HttpHeaders.USER_AGENT, userAgentName);
+				
+//				requestContext.getHeaders().add(HttpHeaders.ACCEPT_ENCODING, "gzip");
 			}
 		});
-		WebTarget target = client.target(urlStanding + event);
+		WebTarget target = client.target(event);
 		Response response = target.request().get();
 		
 		if (response.getStatus() != 200) {
 			throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
 		}
+
+//		InputStream baseJson = new InputStreamReader((InputStream) response.getEntity(), StandardCharsets.UTF_8);
 		
-//		response.getHeaders().getFirst(key)
+//		Standings standings = response.readEntity(Standings.class);
 		
-		Standings standings = (Standings)response.getEntity();
-		
-//		InputStreamReader isr = new InputStreamReader(new ByteArrayInputStream(response.getEntity().getBytes()));
-//		ByteArrayInputStream bais = new ByteArrayInputStream(((String) response.getEntity()).getBytes());
-//		BufferedReader baseJson = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(response.getEntity().getBytes())));
-//
-//		//Read output in string format
-//		String value = response.readEntity(String.class);
-		System.out.println(standings.toString());
-	}	
+		Standings standings = null;
+		try {
+			standings = mapper.readValue(response.readEntity(String.class), Standings.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(standings);
+	}
 }
