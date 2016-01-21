9aa809a3bd09ccf7ec15191144ade225b2cb5227
Paul Rossotti <rossotti.paul@gmail.com>
2016-01-20 17:36:03 -0800

    convert to repository bean

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/client/StandingClient.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/client/StandingClient.java
@@ -1,57 +1,38 @@
 package com.rossotti.basketball.client;
 
-import java.io.IOException;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.ClientRequestContext;
-import javax.ws.rs.client.ClientRequestFilter;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
+
+import org.springframework.stereotype.Repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rossotti.basketball.app.provider.JsonProvider;
-import com.rossotti.basketball.client.dto.Standings;
+import com.rossotti.basketball.client.dto.StandingsDTO;
 
+@Repository
 public class StandingClient {
-	
+
+	private static final String baseUrl = "https://erikberg.com/nba/standings/";
 	private static ObjectMapper mapper = JsonProvider.buildObjectMapper();
 
-	public static void main(String[] args) {
-
-
-		String event = "20130131.json";
-//		String fileStanding = "/home/pablote/pdrive/pwork/play/accumulator/config/fileStanding/2015_16";
-//		Source source = Source.API;
+	public StandingsDTO retrieveStandings(String event) {
+		String standingsUrl = baseUrl + event + ".json";
 
 		Client client = ClientBuilder.newBuilder().build();
-		client.register(new ClientRequestFilter() {
-			@Override
-			public void filter(ClientRequestContext requestContext) throws IOException {
-//				requestContext.getHeaders().add(HttpHeaders.AUTHORIZATION, authHeader);
-//				requestContext.getHeaders().add(HttpHeaders.USER_AGENT, userAgentName);
-				
-//				requestContext.getHeaders().add(HttpHeaders.ACCEPT_ENCODING, "gzip");
-			}
-		});
-		WebTarget target = client.target(event);
-		Response response = target.request().get();
-		
+		client.register(XmlStatsFilter.class);
+		Response response = client.target(standingsUrl).request().get();
+
 		if (response.getStatus() != 200) {
 			throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
 		}
 
-//		InputStream baseJson = new InputStreamReader((InputStream) response.getEntity(), StandardCharsets.UTF_8);
-		
-//		Standings standings = response.readEntity(Standings.class);
-		
-		Standings standings = null;
+		StandingsDTO standings = null;
 		try {
-			standings = mapper.readValue(response.readEntity(String.class), Standings.class);
+			standings = mapper.readValue(response.readEntity(String.class), StandingsDTO.class);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println(standings);
+		return standings;
 	}
 }
