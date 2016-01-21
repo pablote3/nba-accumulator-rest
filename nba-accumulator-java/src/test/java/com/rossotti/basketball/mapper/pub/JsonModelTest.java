987c01d865c8057455a66f428550a9bd6c48d972
Paul Rossotti <rossotti.paul@gmail.com>
2016-01-21 05:54:28 -0800

    move junit dependent json to mockModel

--- nba-accumulator-java/src/test/java/com/rossotti/basketball/mapper/pub/JsonModelTest.java
+++ nba-accumulator-java/src/test/java/com/rossotti/basketball/mapper/pub/JsonModelTest.java
@@ -1,12 +1,7 @@
 package com.rossotti.basketball.mapper.pub;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import org.joda.time.LocalDateTime;
 import org.junit.Assert;
@@ -28,14 +23,10 @@ public class JsonModelTest {
 	@Test
 	public void deserialize_JsonToPojo_Official() {
 		try {
-			Path path =  Paths.get(System.getProperty("config.test")).resolve("official.json");
-			File file = path.toFile();
-			InputStream baseJson = new FileInputStream(file);
+			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/official.json");
 			Official official = mapper.readValue(baseJson, Official.class);
 			Assert.assertEquals("Cash", official.getLastName());
 			baseJson.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -44,14 +35,10 @@ public class JsonModelTest {
 	@Test
 	public void deserialize_JsonToPojo_Team() {
 		try {
-			Path path =  Paths.get(System.getProperty("config.test")).resolve("team.json");
-			File file = path.toFile();
-			InputStream baseJson = new FileInputStream(file);
+			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/team.json");
 			Team team = mapper.readValue(baseJson, Team.class);
 			Assert.assertEquals("TD Garden", team.getSiteName());
 			baseJson.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -60,14 +47,10 @@ public class JsonModelTest {
 	@Test
 	public void deserialize_JsonToPojo_Player() {
 		try {
-			Path path =  Paths.get(System.getProperty("config.test")).resolve("player.json");
-			File file = path.toFile();
-			InputStream baseJson = new FileInputStream(file);
+			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/player.json");
 			Player player = mapper.readValue(baseJson, Player.class);
 			Assert.assertEquals((short)220, player.getWeight().shortValue());
 			baseJson.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -75,47 +58,35 @@ public class JsonModelTest {
 
 	@Test
 	public void deserialize_JsonToPojo_RosterPlayer() {
-		InputStream baseJson;
 		try {
-			Path path =  Paths.get(System.getProperty("config.test")).resolve("rosterPlayer.json");
-			File file = path.toFile();
-			baseJson = new FileInputStream(file);
+			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/rosterPlayer.json");
 			RosterPlayer rosterPlayer = mapper.readValue(baseJson, RosterPlayer.class);
 			Assert.assertEquals(Position.SF, rosterPlayer.getPosition());
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			baseJson.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void deserialize_JsonToPojo_Game() {
-		InputStream baseJson;
 		try {
-			Path path =  Paths.get(System.getProperty("config.test")).resolve("game.json");
-			File file = path.toFile();
-			baseJson = new FileInputStream(file);
+			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/game.json");
 			Game game = mapper.readValue(baseJson, Game.class);
 			Assert.assertEquals(new LocalDateTime("2015-10-28T20:00"), game.getGameDateTime());
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			baseJson.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void deserialize_JsonToPojo_Standing() {
 		try {
-			Path path =  Paths.get(System.getProperty("config.test")).resolve("standing.json");
-			File file = path.toFile();
-			InputStream baseJson = new FileInputStream(file);
+			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/standing.json");
 			Standing standing = mapper.readValue(baseJson, Standing.class);
 			Assert.assertEquals((short)98, standing.getPointsAgainst().shortValue());
 			baseJson.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
