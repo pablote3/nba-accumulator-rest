529216c708ee5eada226fedde93111f92e0cb740
Paul Rossotti <rossotti.paul@gmail.com>
2016-01-20 18:55:16 -0800

    initial commit

---
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/client/GameClient.java
@@ -0,0 +1,37 @@
+package com.rossotti.basketball.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+
+import org.springframework.stereotype.Repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rossotti.basketball.app.provider.JsonProvider;
+import com.rossotti.basketball.client.dto.GameDTO;
+
+@Repository
+public class GameClient {
+
+	private static final String baseUrl = "https://erikberg.com/nba/boxscore/";
+	private static ObjectMapper mapper = JsonProvider.buildObjectMapper();
+
+	public GameDTO retrieveBoxScore(String event) {
+		String boxScoreUrl = baseUrl + event + ".json";
+
+		Client client = ClientBuilder.newBuilder().build();
+		client.register(XmlStatsFilter.class);
+		Response response = client.target(boxScoreUrl).request().get();
+
+		if (response.getStatus() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+		}
+		GameDTO game = null;
+		try {
+			game = mapper.readValue(response.readEntity(String.class), GameDTO.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return game;
+	}
+}
---
+++ nba-accumulator-java/src/test/java/com/rossotti/basketball/client/GameClientTest.java
@@ -0,0 +1,31 @@
+package com.rossotti.basketball.client;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.rossotti.basketball.client.dto.GameDTO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+public class GameClientTest {
+
+	@Autowired
+	private GameClient boxScoreClient;
+
+	@Test
+	public void retrieveBoxScores_Found() {
+		GameDTO game = boxScoreClient.retrieveBoxScore("20151129-detroit-pistons-at-brooklyn-nets");
+		Assert.assertEquals(new DateTime("2015-11-29T23:00:00", DateTimeZone.UTC), game.event_information.getStart_date_time());
+		Assert.assertEquals("Detroit Pistons", game.away_team.getFull_name());
+		Assert.assertEquals(19, game.away_period_scores[1]);
+		Assert.assertTrue(game.away_totals.getPoints().equals((short)83));
+		Assert.assertTrue(game.away_stats[1].getPoints().equals((short)7));
+		Assert.assertEquals("Roe", game.officials[1].getLast_name());
+	}
+}
