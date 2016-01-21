be9e20ec175bec516c979003dddafb766548a1ad
Paul Rossotti <rossotti.paul@gmail.com>
2016-01-20 17:33:38 -0800

    initial commit

---
+++ nba-accumulator-java/src/test/java/com/rossotti/basketball/client/StandingClientTest.java
@@ -0,0 +1,29 @@
+package com.rossotti.basketball.client;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.rossotti.basketball.client.dto.StandingDTO;
+import com.rossotti.basketball.client.dto.StandingsDTO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+public class StandingClientTest {
+
+	@Autowired
+	private StandingClient standingClient;
+
+	@Test
+	public void retrieveStandings_Found() {
+		StandingsDTO standings = standingClient.retrieveStandings("20130131");
+		Assert.assertEquals(new DateTime("2013-01-31T05:00:00", DateTimeZone.UTC), standings.standings_date);
+		StandingDTO standing = standings.standing[1];
+		Assert.assertEquals("5-5", standing.getLast_ten());
+	}
+}
