33434845606c9e83280f770c56e355f725fd6115
Paul Rossotti <rossotti.paul@gmail.com>
2016-02-08 18:18:06 -0800

    condense client tests to ClientBeanTest

--- nba-accumulator-java/src/test/java/com/rossotti/basketball/client/ClientBeanTest.java
+++ nba-accumulator-java/src/test/java/com/rossotti/basketball/client/ClientBeanTest.java
@@ -12,17 +12,32 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:applicationContext.xml"})
-public class RosterClientTest {
-	private static final String baseUrl = "https://erikberg.com/nba/roster/";
+public class ClientBeanTest {
 
 	@Autowired
 	private ClientBean clientBean;
 
 	@Ignore
 	@Test
+	public void retrieveGames() {
+		String boxScoreUrl = "https://erikberg.com/nba/boxscore/20151129-detroit-pistons-at-brooklyn-nets.json";
+		Response response = clientBean.getClient().target(boxScoreUrl).request().get();
+		Assert.assertEquals(200, response.getStatus());
+	}
+
+	@Ignore
+	@Test
 	public void retrieveRoster() {
-		String rosterUrl = baseUrl + "toronto-raptors" + ".json";
+		String rosterUrl = "https://erikberg.com/nba/roster/toronto-raptors.json";
 		Response response = clientBean.getClient().target(rosterUrl).request().get();
+		Assert.assertEquals(200, response.getStatus());
+	}
+
+//	@Ignore
+	@Test
+	public void retrieveStandings() {
+		String standingsUrl = "https://erikberg.com/nba/standings/20130131.json";
+		Response response = clientBean.getClient().target(standingsUrl).request().get();
 		Assert.assertEquals(200, response.getStatus());
 	}
 }
