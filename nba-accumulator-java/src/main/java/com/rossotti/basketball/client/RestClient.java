6a8136db261df0c7809ee367b744824eb7a3a257
Paul Rossotti <rossotti.paul@gmail.com>
2016-02-12 06:12:03 -0800

    initial commit

---
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/client/RestClient.java
@@ -0,0 +1,23 @@
+package com.rossotti.basketball.client;
+
+import java.io.IOException;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
+
+public class RestClient {
+	public static Client buildClient(final String accessToken, final String userAgent) {
+		ClientRequestFilter filter = new ClientRequestFilter() {
+			@Override
+			public void filter(ClientRequestContext requestContext) throws IOException {
+				String authHeader = "Bearer " + accessToken;
+				requestContext.getHeaders().add(HttpHeaders.AUTHORIZATION, authHeader);
+			requestContext.getHeaders().add(HttpHeaders.USER_AGENT, userAgent);
+			}
+		};
+		return ClientBuilder.newBuilder().build().register(filter);
+	}
+}
---
+++ nba-accumulator-java/src/test/java/com/rossotti/basketball/client/RestClientTest.java
@@ -0,0 +1,101 @@
+package com.rossotti.basketball.client;
+
+import java.io.IOException;
+
+import javax.ws.rs.client.Client;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.rossotti.basketball.util.PropertyResourceLoader;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+public class RestClientTest {
+	private Client client;
+	private PropertyResourceLoader resourceProperties;
+
+	@Autowired
+	public void setResourceProperties(PropertyResourceLoader resourceProperties) {
+		this.resourceProperties = resourceProperties;
+	}
+	public PropertyResourceLoader getResourceProperties() {
+		return resourceProperties;
+	}
+
+	@Before
+	public void setUp() {
+		client = null;
+		sleep(10);
+	}
+	
+	@Test
+	public void retrieveRoster_200() throws IOException {
+		String accessToken = getResourceProperties().getProperties().getProperty("xmlstats.accessToken");
+		String userAgent = getResourceProperties().getProperties().getProperty("xmlstats.userAgent");
+		client = RestClient.buildClient(accessToken, userAgent);
+		String rosterUrl = "https://erikberg.com/nba/roster/toronto-raptors.json";
+		int status = client.target(rosterUrl).request().get().getStatus();
+		Assert.assertEquals(200, status);
+	}
+
+	@Test
+	public void retrieveRoster_401() throws IOException {
+		String accessToken = "badToken";
+		String userAgent = getResourceProperties().getProperties().getProperty("xmlstats.userAgent");
+		client = RestClient.buildClient(accessToken, userAgent);
+		String rosterUrl = "https://erikberg.com/nba/roster/toronto-raptors.json";
+		int status = client.target(rosterUrl).request().get().getStatus();
+		Assert.assertEquals(401, status);
+	}
+
+//	@Ignore //could cause ban of IP
+//	@Test
+//	public void retrieveRoster_403() throws IOException {
+//		String accessToken = getResourceProperties().getProperties().getProperty("xmlstats.accessToken");
+//		String userAgent = "badUserAgent";
+//		client = RestClient.buildClient(accessToken, userAgent);
+//		String rosterUrl = "https://erikberg.com/nba/roster/toronto-raptors.json";
+//		int status = client.target(rosterUrl).request().get().getStatus();
+//		Assert.assertEquals(403, status);
+//	}
+
+	@Test
+	public void retrieveRoster_404() throws IOException {
+		String accessToken = getResourceProperties().getProperties().getProperty("xmlstats.accessToken");
+		String userAgent = getResourceProperties().getProperties().getProperty("xmlstats.userAgent");
+		client = RestClient.buildClient(accessToken, userAgent);
+		String rosterUrl = "https://erikberg.com/nba/roster/toronto-raps.json";
+		int status = client.target(rosterUrl).request().get().getStatus();
+		Assert.assertEquals(404, status);
+	}
+
+//	@Ignore //sending more than 6 requests in a minute is counted against account
+//	@Test
+//	public void retrieveRoster_429() throws IOException {
+//		String accessToken = getResourceProperties().getProperties().getProperty("xmlstats.accessToken");
+//		String userAgent = getResourceProperties().getProperties().getProperty("xmlstats.userAgent");
+//		client = RestClient.buildClient(accessToken, userAgent);
+//		String rosterUrl = "https://erikberg.com/nba/roster/toronto-raptors.json";
+//		int status200 = client.target(rosterUrl).request().get().getStatus();
+//		Assert.assertEquals(200, status200);
+//		sleep(1);
+//		client = RestClient.buildClient(accessToken, userAgent);
+//		int status429 = client.target(rosterUrl).request().get().getStatus();
+//		Assert.assertEquals(429, status429);
+//	}
+
+	private void sleep(int nbrSeconds) {
+		try {
+			Thread.sleep(nbrSeconds * 1000);
+		} 
+		catch(InterruptedException ex) {
+			Thread.currentThread().interrupt();
+		}
+	}
+}
