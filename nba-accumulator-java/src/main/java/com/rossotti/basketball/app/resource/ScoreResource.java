38a5ffe9fe07bc86b57705da05d824c2a5b217df
Paul Rossotti <rossotti.paul@gmail.com>
2016-02-22 18:29:44 -0800

    introduce logger

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/app/resource/ScoreResource.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/app/resource/ScoreResource.java
@@ -10,14 +10,18 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.rossotti.basketball.model.Game;
+import com.rossotti.basketball.model.GameStatus;
 import com.rossotti.basketball.pub.PubGame;
 
 @Service
 @Path("/score/games")
 public class ScoreResource {
+	private final Logger logger = LoggerFactory.getLogger(ScoreResource.class);
 
 	@POST
 	@Path("/{gameDate}/{teamKey}")
@@ -28,18 +32,25 @@ public class ScoreResource {
 								@PathParam("teamKey") String teamKey, 
 								Game game) {
 
-//			if (game.getStatus().equals(Game.Status.Scheduled)) {
-//				
-//			}
-//			else {
-//				
-//			}
-//			
+		StringBuilder event = new StringBuilder();
+		event.append(gameDateString + "-");
+		event.append(game.getBoxScores().get(0).getTeam().getTeamKey() + "-at-");
+		event.append(game.getBoxScores().get(1).getTeam().getTeamKey());
+
+		if (game.getStatus().equals(GameStatus.Scheduled)) {
+			logger.info('\n' + "Scheduled game ready to be scored: " + event);
+		
 			PubGame pubGame = game.toPubGame(uriInfo, teamKey);
 			
 			return Response.ok(pubGame)
 				.link(uriInfo.getAbsolutePath(), "game")
 				.build();
-
+		}
+		else {
+			logger.info('\n' + "" + game.getStatus() + " game not eligible to be scored: " + event);
+			return Response.notModified()
+					.link(uriInfo.getAbsolutePath(), "game")
+					.build();
+		}
 	}
 }
