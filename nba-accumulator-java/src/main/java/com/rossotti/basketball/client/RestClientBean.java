f7997898ff794eebbf694c9e2bf26c9b7e8d2ebc
Paul Rossotti <rossotti.paul@gmail.com>
2016-03-21 11:02:32 -0700

    enhance error handling and property driven urls

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/client/RestClientBean.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/client/RestClientBean.java
@@ -1,12 +1,17 @@
 package com.rossotti.basketball.client;
 
+import java.io.IOException;
+
 import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rossotti.basketball.app.provider.JsonProvider;
+import com.rossotti.basketball.app.resource.PropertyBean;
 import com.rossotti.basketball.client.dto.GameDTO;
 import com.rossotti.basketball.client.dto.RosterDTO;
 import com.rossotti.basketball.client.dto.StandingsDTO;
@@ -16,6 +21,9 @@ import com.rossotti.basketball.client.dto.StatsDTO;
 public class RestClientBean {
 	@Autowired
 	private ClientBean clientBean;
+
+	@Autowired
+	private PropertyBean propertyBean;
 
 	private static ObjectMapper mapper = JsonProvider.buildObjectMapper();
 
@@ -27,8 +35,15 @@ public class RestClientBean {
 		} else {
 			try {
 				statsDTO = mapper.readValue(response.readEntity(String.class), statsDTO.getClass());
-			} catch (Exception e) {
-				e.printStackTrace();
+			} catch (JsonParseException jpe) {
+				statsDTO.httpStatus = 500;
+				jpe.printStackTrace();
+			} catch (JsonMappingException jme) {
+				statsDTO.httpStatus = 500;
+				jme.printStackTrace();
+			} catch (IOException ioe) {
+				statsDTO.httpStatus = 500;
+				ioe.printStackTrace();
 			}
 		}
 		statsDTO.httpStatus = response.getStatus();
@@ -37,21 +52,24 @@ public class RestClientBean {
 	}
 
 	public GameDTO retrieveBoxScore(String event) {
-		GameDTO gameDTO = new GameDTO();;
-		String boxScoreUrl = "https://erikberg.com/nba/boxscore/" + event + ".json";
-		return (GameDTO)retrieveStats(boxScoreUrl, gameDTO);
+		String url = propertyBean.getProperty_Http("xmlstats.urlBoxScore");
+		String eventUrl = url + event + ".json";
+		GameDTO dto = new GameDTO();
+		return (GameDTO)retrieveStats(eventUrl, dto);
 	}
 
 	public RosterDTO retrieveRoster(String event) {
-		RosterDTO rosterDTO = new RosterDTO();
-		String rosterUrl = "https://erikberg.com/nba/roster/" + event + ".json";
-		return (RosterDTO)retrieveStats(rosterUrl, rosterDTO);
+		String url = propertyBean.getProperty_Http("xmlstats.urlRoster");
+		String eventUrl = url + event + ".json";
+		RosterDTO dto = new RosterDTO();
+		return (RosterDTO)retrieveStats(eventUrl, dto);
 	}
 
 	public StandingsDTO retrieveStandings(String event) {
-		StandingsDTO standingsDTO = new StandingsDTO();
-		String standingsUrl = "https://erikberg.com/nba/standings/" + event + ".json";
-		return (StandingsDTO)retrieveStats(standingsUrl, standingsDTO);
+		String url = propertyBean.getProperty_Http("xmlstats.urlStandings");
+		String eventUrl = url + event + ".json";
+		StandingsDTO dto = new StandingsDTO();
+		return (StandingsDTO)retrieveStats(eventUrl, dto);
 	}
 
 	public ClientBean getClientBean() {
