ccdb4c5f6850b7a200d0c8f94764fdbd6e1656ff
Paul Rossotti <rossotti.paul@gmail.com>
2016-03-21 13:23:46 -0700

    initial commit

---
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/client/FileClientBean.java
@@ -0,0 +1,85 @@
+package com.rossotti.basketball.client;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rossotti.basketball.app.provider.JsonProvider;
+import com.rossotti.basketball.app.resource.PropertyBean;
+import com.rossotti.basketball.client.dto.GameDTO;
+import com.rossotti.basketball.client.dto.RosterDTO;
+import com.rossotti.basketball.client.dto.StandingsDTO;
+import com.rossotti.basketball.client.dto.StatsDTO;
+
+@Service
+public class FileClientBean {
+	@Autowired
+	private PropertyBean propertyBean;
+
+	private static ObjectMapper mapper = JsonProvider.buildObjectMapper();
+
+	private StatsDTO retrieveStats(String stringPath, String event, StatsDTO statsDTO) {
+		String stringFile = event + ".json";
+		Path path = Paths.get(stringPath).resolve(stringFile);
+		InputStreamReader baseJson = null;
+		try {
+			File file = path.toFile();
+			InputStream inputStreamJson = new FileInputStream(file);
+			baseJson = new InputStreamReader(inputStreamJson, StandardCharsets.UTF_8);
+			statsDTO = mapper.readValue(baseJson, statsDTO.getClass());
+			statsDTO.httpStatus = 200;
+		} catch (FileNotFoundException fnf) {
+			statsDTO.httpStatus = 404;
+			fnf.printStackTrace();
+		} catch (JsonParseException jpe) {
+			statsDTO.httpStatus = 500;
+			jpe.printStackTrace();
+		} catch (JsonMappingException jme) {
+			statsDTO.httpStatus = 500;
+			jme.printStackTrace();
+		} catch (IOException ioe) {
+			statsDTO.httpStatus = 500;
+			ioe.printStackTrace();
+		}
+		finally {
+			try {
+				if (baseJson != null)
+					baseJson.close();
+			} catch (IOException ioe) {
+				statsDTO.httpStatus = 500;
+				ioe.printStackTrace();
+			}
+		}
+		return statsDTO;
+	}
+
+	public GameDTO retrieveBoxScore(String event) {
+		String path = propertyBean.getProperty_Path("xmlstats.fileBoxScore");
+		GameDTO dto = new GameDTO();
+		return (GameDTO)retrieveStats(path, event, dto);
+	}
+
+	public RosterDTO retrieveRoster(String event) {
+		String path = propertyBean.getProperty_Path("xmlstats.fileRoster");
+		RosterDTO dto = new RosterDTO();
+		return (RosterDTO)retrieveStats(path, event, dto);
+	}
+
+	public StandingsDTO retrieveStandings(String event) {
+		String path = propertyBean.getProperty_Path("xmlstats.fileStandings");
+		StandingsDTO dto = new StandingsDTO();
+		return (StandingsDTO)retrieveStats(path, event, dto);
+	}
+}
---
+++ nba-accumulator-java/src/test/java/com/rossotti/basketball/client/FileClientTest.java
@@ -0,0 +1,63 @@
+package com.rossotti.basketball.client;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.rossotti.basketball.client.dto.GameDTO;
+import com.rossotti.basketball.client.dto.RosterDTO;
+import com.rossotti.basketball.client.dto.StandingsDTO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+public class FileClientTest {
+	@Autowired
+	private FileClientBean fileClientBean;
+
+	@Test
+	public void retrieveBoxScore_200() throws IOException {
+		String event = "20160311-houston-rockets-at-boston-celtics";
+		GameDTO game = fileClientBean.retrieveBoxScore(event);
+		Assert.assertEquals(200, game.httpStatus);
+		Assert.assertEquals("Houston Rockets", game.away_team.getFull_name());
+	}
+	@Test
+	public void retrieveBoxScore_404() throws IOException {
+		String event = "20160311-houston-rockets-at-boston-celticsss";
+		GameDTO game = fileClientBean.retrieveBoxScore(event);
+		Assert.assertEquals(404, game.httpStatus);
+	}
+
+	@Test
+	public void retrieveRoster_200() throws IOException {
+		String event = "toronto-raptors";
+		RosterDTO roster = fileClientBean.retrieveRoster(event);
+		Assert.assertEquals(200, roster.httpStatus);
+		Assert.assertEquals("Toronto Raptors", roster.team.getFull_name());
+	}
+	@Test
+	public void retrieveRoster_404() throws IOException {
+		String event = "toronto-raptiers";
+		RosterDTO roster = fileClientBean.retrieveRoster(event);
+		Assert.assertEquals(404, roster.httpStatus);
+	}
+
+	@Test
+	public void retrieveStandings_200() throws IOException {
+		String event = "20160216";
+		StandingsDTO standings = fileClientBean.retrieveStandings(event);
+		Assert.assertEquals(200, standings.httpStatus);
+		Assert.assertEquals("cleveland-cavaliers", standings.standing[0].getTeam_id());
+	}
+	@Test
+	public void retrieveStandings_404() throws IOException {
+		String event = "20160236";
+		StandingsDTO standings = fileClientBean.retrieveStandings(event);
+		Assert.assertEquals(404, standings.httpStatus);
+	}
+}
