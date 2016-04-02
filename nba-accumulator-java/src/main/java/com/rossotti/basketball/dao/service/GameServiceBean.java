c1b92e09da9d422bee8175048f57aea18743a8e4
Paul Rossotti <rossotti.paul@gmail.com>
2016-04-02 16:48:47 -0700

    added updateGame()

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/dao/service/GameServiceBean.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/dao/service/GameServiceBean.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rossotti.basketball.dao.GameDAO;
+import com.rossotti.basketball.dao.model.Game;
 
 @Service
 public class GameServiceBean {
@@ -14,5 +15,9 @@ public class GameServiceBean {
 
 	public LocalDateTime findPreviousGameDateTime(LocalDate gameDate, String teamKey) {
 		return gameDAO.findPreviousGameDateTimeByDateTeam(gameDate, teamKey);
+	}
+
+	public Game updateGame(Game game) {
+		return gameDAO.updateGame(game);
 	}
 }
