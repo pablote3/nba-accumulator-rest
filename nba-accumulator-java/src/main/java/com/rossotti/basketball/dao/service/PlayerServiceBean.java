4304ac05b1021c6732344b89cd2318acbb6835e0
Paul Rossotti <rossotti.paul@gmail.com>
2016-04-04 19:16:10 -0700

    initial commit

---
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/dao/service/PlayerServiceBean.java
@@ -0,0 +1,18 @@
+package com.rossotti.basketball.dao.service;
+
+import org.joda.time.LocalDate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rossotti.basketball.dao.PlayerDAO;
+import com.rossotti.basketball.dao.model.Player;
+
+@Service
+public class PlayerServiceBean {
+	@Autowired
+	private PlayerDAO playerDAO;
+
+	public Player findByPlayerNameBirthdate(String lastName, String firstName, LocalDate birthdate) {
+		return playerDAO.findPlayer(lastName, firstName, birthdate);
+	}
+}
