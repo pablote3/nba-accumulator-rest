62a0ac0f9329fd62a66173de69498c40b43a5787
Paul Rossotti <rossotti.paul@gmail.com>
2016-04-05 06:23:14 -0700

    expand roster management

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/app/resource/RosterResource.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/app/resource/RosterResource.java
@@ -23,6 +23,7 @@ import com.rossotti.basketball.client.FileClientBean;
 import com.rossotti.basketball.client.RestClientBean;
 import com.rossotti.basketball.client.dto.RosterDTO;
 import com.rossotti.basketball.client.dto.RosterPlayerDTO;
+import com.rossotti.basketball.dao.exception.DuplicateEntityException;
 import com.rossotti.basketball.dao.exception.NoSuchEntityException;
 import com.rossotti.basketball.dao.model.BoxScore;
 import com.rossotti.basketball.dao.model.BoxScore.Result;
@@ -32,11 +33,13 @@ import com.rossotti.basketball.dao.model.Official;
 import com.rossotti.basketball.dao.model.Player;
 import com.rossotti.basketball.dao.model.RosterPlayer;
 import com.rossotti.basketball.dao.pub.PubGame;
+import com.rossotti.basketball.dao.pub.PubRosterPlayers_ByTeam;
 import com.rossotti.basketball.dao.service.GameServiceBean;
 import com.rossotti.basketball.dao.service.OfficialServiceBean;
 import com.rossotti.basketball.dao.service.PlayerServiceBean;
 import com.rossotti.basketball.dao.service.RosterPlayerServiceBean;
 import com.rossotti.basketball.util.DateTimeUtil;
+import com.rossotti.basketball.util.FormatUtil;
 
 @Service
 @Path("/score/roster")
@@ -87,6 +90,7 @@ public class RosterResource {
 				List<RosterPlayer> activeRosterPlayers = rosterPlayerServiceBean.getRosterPlayers(rosterDTO.players, fromDate, teamKey);
 				RosterPlayer finderRosterPlayer;
 				Player finderPlayer;
+				StringBuilder sb;
 				for (int i = 0; i < activeRosterPlayers.size(); i++) {
 					RosterPlayer activeRosterPlayer = activeRosterPlayers.get(i);
 					Player activePlayer = activeRosterPlayer.getPlayer();
@@ -99,53 +103,61 @@ public class RosterResource {
 							finderPlayer = playerServiceBean.findByPlayerNameBirthdate(activePlayer.getLastName(), activePlayer.getFirstName(), activePlayer.getBirthdate());
 							if (finderPlayer.isNotFound()) {
 								//player does not exist
-								
+								Player createPlayer = playerServiceBean.createPlayer(activePlayer);
+								activeRosterPlayer.setPlayer(createPlayer);
+								activeRosterPlayer.setFromDate(fromDate);
+								activeRosterPlayer.setToDate(toDate);
+								rosterPlayerServiceBean.createRosterPlayer(activeRosterPlayer);
+								logger.info(generateLogMessage("Player does not exist", activeRosterPlayer));
+							}
+							else {
+								//player does exist, not on any roster
+								activeRosterPlayer.setPlayer(finderPlayer);
+								activeRosterPlayer.setFromDate(fromDate);
+								activeRosterPlayer.setToDate(toDate);
+								rosterPlayerServiceBean.createRosterPlayer(activeRosterPlayer);
+								logger.info(generateLogMessage("Player does exist, not on any roster", activeRosterPlayer));
 							}
 						}
-					}
+						else {
+							//player is on another roster for current season
+							finderRosterPlayer.setToDate(DateTimeUtil.getDateMinusOneDay(fromDate));
+							rosterPlayerServiceBean.updateRosterPlayer(finderRosterPlayer);
+							logger.info(generateLogMessage("Player on another team -  " + finderRosterPlayer.getTeam().getAbbr() + " - Terminate", finderRosterPlayer));
 
-//					game.setGameOfficials(officialServiceBean.getGameOfficials(rosterDTO.officials, gameDate));
-//
-//					if (rosterDTO.away_totals.getPoints() > rosterDTO.home_totals.getPoints()) {
-//						awayBoxScore.setResult(Result.Win);
-//						homeBoxScore.setResult(Result.Loss);
-//					}
-//					else {
-//						awayBoxScore.setResult(Result.Loss);
-//						homeBoxScore.setResult(Result.Win);
-//					}
-//
-//					awayBoxScore.setDaysOff((short)DateTimeUtil.getDaysBetweenTwoDateTimes(gameServiceBean.findPreviousGameDateTime(gameDate, awayTeamKey), gameDateTime));
-//					homeBoxScore.setDaysOff((short)DateTimeUtil.getDaysBetweenTwoDateTimes(gameServiceBean.findPreviousGameDateTime(gameDate, homeTeamKey), gameDateTime));
-//
-//					Game updatedGame = gameServiceBean.updateGame(game);
-//					if (updatedGame.isUpdated()) {
-//						logger.info("Game Scored " + awayTeamKey +  " " + awayBoxScore.getPoints() + " " + homeTeamKey +  " " + homeBoxScore.getPoints());
-//					}
-//					else {
-//						logger.info("Unable to update game - " + updatedGame.getStatus());
-//					}
-//				} catch (NoSuchEntityException nse) {
-//					if (nse.getEntityClass().equals(Official.class)) {
-//						logger.info("Official not found");
-//					}
-//					else if (nse.getEntityClass().equals(RosterPlayer.class)) {
-//						logger.info("Rebuild active roster");
-//					}
-//				} catch (Exception e) {
-//					e.printStackTrace();
+							activeRosterPlayer.setPlayer(finderPlayer);
+							activeRosterPlayer.setFromDate(fromDate);
+							activeRosterPlayer.setToDate(toDate);
+							rosterPlayerServiceBean.createRosterPlayer(activeRosterPlayer);
+							logger.info(generateLogMessage("Player on another team -  " + activeRosterPlayer.getTeam().getAbbr() + " - Add", activeRosterPlayer));
+						}
+					}
+					else {
+						//player is on current team roster
+						logger.info(generateLogMessage("Player on current team roster -  ", activeRosterPlayer));
+					}
 				}
 			}
-				
-//			PubGame pubGame = game.toPubGame(uriInfo, teamKey);
-//				
-//			return Response.ok(pubGame)
-//				.link(uriInfo.getAbsolutePath(), "game")
-//				.build();
+
+			PubRosterPlayers_ByTeam pubRosterPlayers = new PubRosterPlayers_ByTeam(uriInfo.getAbsolutePath(), pubTeam, listPubRosterPlayers);
+			return Response.ok(pubRosterPlayers)
+				.link(uriInfo.getAbsolutePath(), "rosterPlayer")
+				.build();
 		}
 		catch (Exception e) {
 			System.out.println("exception = " + e);
 			return null;
 		}
+	}
+
+	private String generateLogMessage(String messageType, RosterPlayer rosterPlayer) {
+		StringBuilder sb;
+		sb = new StringBuilder();
+		sb.append(FormatUtil.padString(messageType, 40));
+		sb.append(" name = " + FormatUtil.padString(rosterPlayer.getPlayer().getFirstName() + " " + rosterPlayer.getPlayer().getLastName(), 35));
+		sb.append(" dob = " + DateTimeUtil.getStringDate(rosterPlayer.getPlayer().getBirthdate()));
+		sb.append(" fromDate = " + DateTimeUtil.getStringDate(rosterPlayer.getFromDate()));
+		sb.append(" toDate = " + DateTimeUtil.getStringDate(rosterPlayer.getToDate()));
+		return sb.toString();
 	}
 }
