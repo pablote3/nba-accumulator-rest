0e275eb07aa444e5953ac13dc011a6bd37dce967
Paul Rossotti <rossotti.paul@gmail.com>
2016-04-09 23:38:58 -0700

    switch try/catch to throws IOException

--- nba-accumulator-java/src/test/java/com/rossotti/basketball/client/dto/ObjectMapperTest.java
+++ nba-accumulator-java/src/test/java/com/rossotti/basketball/client/dto/ObjectMapperTest.java
@@ -16,45 +16,33 @@ public class ObjectMapperTest {
 	private ObjectMapper mapper = JsonProvider.buildObjectMapper();
 
 	@Test
-	public void deserialize_JsonToPojo_Roster() {
-		try {
-			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockClient/rosterClient.json");
-			RosterDTO roster = mapper.readValue(baseJson, RosterDTO.class);
-			Assert.assertEquals("toronto-raptors", roster.team.getTeam_id());
-			Assert.assertEquals("São Gonçalo, Brazil", roster.players[8].getBirthplace());
-			Assert.assertEquals("Valančiūnas", roster.players[13].getLast_name());
-			baseJson.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void deserialize_JsonToPojo_Roster() throws IOException {
+		InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockClient/rosterClient.json");
+		RosterDTO roster = mapper.readValue(baseJson, RosterDTO.class);
+		Assert.assertEquals("toronto-raptors", roster.team.getTeam_id());
+		Assert.assertEquals("São Gonçalo, Brazil", roster.players[8].getBirthplace());
+		Assert.assertEquals("Valančiūnas", roster.players[13].getLast_name());
+		baseJson.close();
 	}
 
 	@Test
-	public void deserialize_JsonToPojo_Game() {
-		try {
-			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockClient/gameClient.json");
-			GameDTO game = mapper.readValue(baseJson, GameDTO.class);
-			Assert.assertEquals("detroit-pistons", game.away_team.getTeam_id());
-			Assert.assertEquals(17, game.home_period_scores[1]);
-			Assert.assertEquals("Anthony Tolliver", game.away_stats[6].getDisplay_name());
-			Assert.assertEquals("Zarba", game.officials[0].getLast_name());
-			Assert.assertEquals("completed", game.event_information.getStatus());
-			Assert.assertTrue(game.away_totals.getThree_point_field_goals_attempted().equals((short)24));
-			baseJson.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void deserialize_JsonToPojo_Game() throws IOException {
+		InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockClient/gameClient.json");
+		GameDTO game = mapper.readValue(baseJson, GameDTO.class);
+		Assert.assertEquals("detroit-pistons", game.away_team.getTeam_id());
+		Assert.assertEquals(17, game.home_period_scores[1]);
+		Assert.assertEquals("Anthony Tolliver", game.away_stats[6].getDisplay_name());
+		Assert.assertEquals("Zarba", game.officials[0].getLast_name());
+		Assert.assertEquals("completed", game.event_information.getStatus());
+		Assert.assertTrue(game.away_totals.getThree_point_field_goals_attempted().equals((short)24));
+		baseJson.close();
 	}
 
 	@Test
-	public void deserialize_JsonToPojo_Standings() {
-		try {
-			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockClient/standingsClient.json");
-			StandingsDTO standings = mapper.readValue(baseJson, StandingsDTO.class);
-			Assert.assertEquals("toronto-raptors", standings.standing[1].getTeam_id());
-			baseJson.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void deserialize_JsonToPojo_Standings() throws IOException {
+		InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockClient/standingsClient.json");
+		StandingsDTO standings = mapper.readValue(baseJson, StandingsDTO.class);
+		Assert.assertEquals("toronto-raptors", standings.standing[1].getTeam_id());
+		baseJson.close();
 	}
 }
--- nba-accumulator-java/src/test/java/com/rossotti/basketball/dao/model/ModelMapperTest.java
+++ nba-accumulator-java/src/test/java/com/rossotti/basketball/dao/model/ModelMapperTest.java
@@ -21,74 +21,50 @@ public class ModelMapperTest {
 	private ObjectMapper mapper = JsonProvider.buildObjectMapper();
 
 	@Test
-	public void deserialize_JsonToPojo_Official() {
-		try {
-			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/officialModel.json");
-			Official official = mapper.readValue(baseJson, Official.class);
-			Assert.assertEquals("Cash", official.getLastName());
-			baseJson.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void deserialize_JsonToPojo_Official() throws IOException {
+		InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/officialModel.json");
+		Official official = mapper.readValue(baseJson, Official.class);
+		Assert.assertEquals("Cash", official.getLastName());
+		baseJson.close();
 	}
 
 	@Test
-	public void deserialize_JsonToPojo_Team() {
-		try {
-			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/teamModel.json");
-			Team team = mapper.readValue(baseJson, Team.class);
-			Assert.assertEquals("TD Garden", team.getSiteName());
-			baseJson.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void deserialize_JsonToPojo_Team() throws IOException {
+		InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/teamModel.json");
+		Team team = mapper.readValue(baseJson, Team.class);
+		Assert.assertEquals("TD Garden", team.getSiteName());
+		baseJson.close();
 	}
 
 	@Test
-	public void deserialize_JsonToPojo_Player() {
-		try {
-			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/playerModel.json");
-			Player player = mapper.readValue(baseJson, Player.class);
-			Assert.assertEquals((short)220, player.getWeight().shortValue());
-			baseJson.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void deserialize_JsonToPojo_Player() throws IOException {
+		InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/playerModel.json");
+		Player player = mapper.readValue(baseJson, Player.class);
+		Assert.assertEquals((short)220, player.getWeight().shortValue());
+		baseJson.close();
 	}
 
 	@Test
-	public void deserialize_JsonToPojo_RosterPlayer() {
-		try {
-			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/rosterPlayerModel.json");
-			RosterPlayer rosterPlayer = mapper.readValue(baseJson, RosterPlayer.class);
-			Assert.assertEquals(Position.SF, rosterPlayer.getPosition());
-			baseJson.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void deserialize_JsonToPojo_RosterPlayer() throws IOException {
+		InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/rosterPlayerModel.json");
+		RosterPlayer rosterPlayer = mapper.readValue(baseJson, RosterPlayer.class);
+		Assert.assertEquals(Position.SF, rosterPlayer.getPosition());
+		baseJson.close();
 	}
 
 	@Test
-	public void deserialize_JsonToPojo_Game() {
-		try {
-			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/gameModel.json");
-			Game game = mapper.readValue(baseJson, Game.class);
-			Assert.assertEquals(new LocalDateTime("2015-10-28T20:00"), game.getGameDateTime());
-			baseJson.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void deserialize_JsonToPojo_Game() throws IOException {
+		InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/gameModel.json");
+		Game game = mapper.readValue(baseJson, Game.class);
+		Assert.assertEquals(new LocalDateTime("2015-10-28T20:00"), game.getGameDateTime());
+		baseJson.close();
 	}
 
 	@Test
-	public void deserialize_JsonToPojo_Standing() {
-		try {
-			InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/standingModel.json");
-			Standing standing = mapper.readValue(baseJson, Standing.class);
-			Assert.assertEquals((short)98, standing.getPointsAgainst().shortValue());
-			baseJson.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void deserialize_JsonToPojo_Standing() throws IOException {
+		InputStream baseJson = this.getClass().getClassLoader().getResourceAsStream("mockModel/standingModel.json");
+		Standing standing = mapper.readValue(baseJson, Standing.class);
+		Assert.assertEquals((short)98, standing.getPointsAgainst().shortValue());
+		baseJson.close();
 	}
 }
