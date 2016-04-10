5b1d5e5344687421a3908d8463c4e6558a11f626
Paul Rossotti <rossotti.paul@gmail.com>
2016-04-09 23:58:41 -0700

    enhance deserializeGame() assertions

--- nba-accumulator-java/src/test/java/com/rossotti/basketball/client/dto/ObjectMapperTest.java
+++ nba-accumulator-java/src/test/java/com/rossotti/basketball/client/dto/ObjectMapperTest.java
@@ -31,7 +31,8 @@ public class ObjectMapperTest {
 		GameDTO game = mapper.readValue(baseJson, GameDTO.class);
 		Assert.assertEquals("detroit-pistons", game.away_team.getTeam_id());
 		Assert.assertEquals(17, game.home_period_scores[1]);
-		Assert.assertEquals("Anthony Tolliver", game.away_stats[6].getDisplay_name());
+		Assert.assertEquals("Bojan Bogdanović", game.home_stats[6].getDisplay_name());
+		Assert.assertEquals(0f, (float)game.home_stats[6].getFree_throw_percentage(), 0.0f);
 		Assert.assertEquals("Zarba", game.officials[0].getLast_name());
 		Assert.assertEquals("completed", game.event_information.getStatus());
 		Assert.assertTrue(game.away_totals.getThree_point_field_goals_attempted().equals((short)24));
