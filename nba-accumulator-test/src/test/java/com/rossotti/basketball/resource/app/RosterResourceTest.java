5201952f17b6f3dd0c73563743cb6267b34785a2
Paul Rossotti <rossotti.paul@gmail.com>
2016-05-12 05:48:44 -0700

    additional test cases

--- nba-accumulator-test/src/test/java/com/rossotti/basketball/resource/app/RosterResourceTest.java
+++ nba-accumulator-test/src/test/java/com/rossotti/basketball/resource/app/RosterResourceTest.java
@@ -2,10 +2,6 @@ package com.rossotti.basketball.resource.app;
 
 import static com.jayway.restassured.RestAssured.expect;
 
-import javax.json.Json;
-import javax.json.JsonBuilderFactory;
-import javax.json.JsonObject;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,49 +15,45 @@ public class RosterResourceTest {
 	}
 
 	@Test
-	public void manageRoster_GameScheduled() {
+	public void manageRoster_RosterPlayersExist() {
 		expect().
 			statusCode(200).
 		given().
 			contentType(ContentType.JSON).
-			body(postJsonGame("2015-04-16T20:00", "atlanta-hawks", "houston-rockets").toString()).
-			when().
-			post("/roster");
+		when().
+			post("/roster/2015-04-12/toronto-raptors");
 	}
 
-//	@Test
-//	public void scoreGame_GameNotFound() {
-//		expect().
-//			statusCode(404).
-//		given().
-//			contentType(ContentType.JSON).
-//			body(postJsonGame("2015-04-16T20:00", "portland-trail-blazers", "denver-nuggets").toString()).
-//			when().
-//			post("/games");
-//	}
-//
-//	@Test
-//	public void scoreGame_OfficialNotFound() {
-//		expect().
-//			statusCode(500).
-//		given().
-//			contentType(ContentType.JSON).
-//			body(postJsonGame("2015-04-16T20:00", "phoenix-suns", "miami-heat").toString()).
-//			when().
-//			post("/games");
-//	}
-//
-//	@Test
-//	public void scoreGame_RosterPlayerNotFound() {
-//		expect().
-//			statusCode(500).
-//		given().
-//			contentType(ContentType.JSON).
-//			body(postJsonGame("2015-04-16T20:00", "indiana-pacers", "orlando-magic").toString()).
-//			when().
-//			post("/games");
-//	}
-//
+	@Test
+	public void manageRoster_PlayerOnAnotherTeamRoster() {
+		expect().
+		statusCode(200).
+	given().
+		contentType(ContentType.JSON).
+	when().
+		post("/roster/2015-04-12/utah-jazz");
+	}
+
+	@Test
+	public void manageRoster_PlayerNotOnAnyTeamRoster() {
+		expect().
+		statusCode(200).
+	given().
+		contentType(ContentType.JSON).
+	when().
+		post("/roster/2015-04-12/san-antonio-spurs");
+	}
+
+	@Test
+	public void manageRoster_PlayerDoesNotExist() {
+		expect().
+		statusCode(200).
+	given().
+		contentType(ContentType.JSON).
+	when().
+		post("/roster/2015-04-12/brooklyn-nets");
+	}
+
 //	@Test
 //	public void scoreGame_FileNotFound() {
 //		expect().
@@ -83,23 +75,4 @@ public class RosterResourceTest {
 //			when().
 //			post("/games");
 //	}
-
-	private static JsonObject postJsonGame(String gameDateTime, String homeTeamKey, String awayTeamKey) {
-		JsonBuilderFactory factory = Json.createBuilderFactory(null);
-		JsonObject value = factory.createObjectBuilder()
-			.add("gameDateTime", gameDateTime)
-			.add("status", "Scheduled")
-			.add("seasonType", "Regular")
-			.add("boxScores", factory.createArrayBuilder()
-				.add(factory.createObjectBuilder()
-					.add("location", "Home")
-					.add("team", factory.createObjectBuilder()
-						.add("teamKey", homeTeamKey)))
-				.add(factory.createObjectBuilder()
-					.add("location", "Away")
-					.add("team", factory.createObjectBuilder()
-						.add("teamKey", awayTeamKey))))
-		.build();
-		return value;
-	}
 }
