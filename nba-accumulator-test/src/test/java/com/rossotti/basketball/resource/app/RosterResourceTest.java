5cbae4e6a7fada054ba9742398a68a067f6ece22
Paul Rossotti <rossotti.paul@gmail.com>
2016-05-13 05:51:33 -0700

    complete unit tests

--- nba-accumulator-test/src/test/java/com/rossotti/basketball/resource/app/RosterResourceTest.java
+++ nba-accumulator-test/src/test/java/com/rossotti/basketball/resource/app/RosterResourceTest.java
@@ -47,32 +47,30 @@ public class RosterResourceTest {
 	@Test
 	public void manageRoster_PlayerDoesNotExist() {
 		expect().
-		statusCode(200).
-	given().
-		contentType(ContentType.JSON).
-	when().
-		post("/roster/2015-04-12/brooklyn-nets");
+			statusCode(200).
+		given().
+			contentType(ContentType.JSON).
+		when().
+			post("/roster/2015-04-12/brooklyn-nets");
 	}
 
-//	@Test
-//	public void scoreGame_FileNotFound() {
-//		expect().
-//			statusCode(404).
-//		given().
-//			contentType(ContentType.JSON).
-//			body(postJsonGame("2015-04-16T20:00", "toronto-raptors", "new-york-knicks").toString()).
-//			when().
-//			post("/games");
-//	}
-//
-//	@Test
-//	public void scoreGame_GameCompleted() {
-//		expect().
-//			statusCode(500).
-//		given().
-//			contentType(ContentType.JSON).
-//			body(postJsonGame("2015-04-15T20:00", "chicago-bulls", "atlanta-hawks").toString()).
-//			when().
-//			post("/games");
-//	}
+	@Test
+	public void manageRoster_DeactivateInactivePlayer() {
+		expect().
+			statusCode(200).
+		given().
+			contentType(ContentType.JSON).
+		when().
+			post("/roster/2015-04-12/chicago-bulls");
+	}
+
+	@Test
+	public void manageRoster_FileNotFound() {
+		expect().
+			statusCode(404).
+		given().
+			contentType(ContentType.JSON).
+		when().
+			post("/roster/2015-04-12/sacramento-kings");
+		}
 }
