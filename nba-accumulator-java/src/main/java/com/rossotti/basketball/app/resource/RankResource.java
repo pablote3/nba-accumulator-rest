1abc227460daa86524da09cbe7b0cde0bab6415c
Paul Rossotti <rossotti.paul@gmail.com>
2016-05-15 17:23:49 -0700

    comment unneeded log statements

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/app/resource/RankResource.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/app/resource/RankResource.java
@@ -94,10 +94,10 @@ public class RankResource {
 					if (createdStanding.isCreated()) {
 						BigDecimal opponentRecord = standingRecord.getGamesPlayed() == 0 ? new BigDecimal(0) : new BigDecimal(standingRecord.getGamesWon()).divide(new BigDecimal(standingRecord.getGamesPlayed()), 4, RoundingMode.HALF_UP);
 						BigDecimal opponentOpponentRecord = standingRecord.getOpptGamesPlayed() == 0 ? new BigDecimal(0) : new BigDecimal(standingRecord.getOpptGamesWon()).divide(new BigDecimal(standingRecord.getOpptGamesPlayed()), 4, RoundingMode.HALF_UP);
-						logger.error("    Opponent Games Won/Played = " + standingRecord.getGamesWon() + "-" + standingRecord.getGamesPlayed());
-						logger.error("    OpptOppt Games Won/Played = " + standingRecord.getOpptGamesWon() + "-" + standingRecord.getOpptGamesPlayed());
-						logger.error("    Opponent Record = " + opponentRecord);
-						logger.error("    OpptOppt Record = " + opponentOpponentRecord);
+//						logger.error("    Opponent Games Won/Played = " + standingRecord.getGamesWon() + "-" + standingRecord.getGamesPlayed());
+//						logger.error("    OpptOppt Games Won/Played = " + standingRecord.getOpptGamesWon() + "-" + standingRecord.getOpptGamesPlayed());
+//						logger.error("    Opponent Record = " + opponentRecord);
+//						logger.error("    OpptOppt Record = " + opponentOpponentRecord);
 						logger.info("  Strenghth Of Schedule  " + teamKey + " " + opponentRecord.multiply(new BigDecimal(2)).add(opponentOpponentRecord).divide(new BigDecimal(3), 4, RoundingMode.HALF_UP));
 					}
 					else {
