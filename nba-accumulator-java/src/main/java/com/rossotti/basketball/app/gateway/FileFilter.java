f67549cf389d890c4fcba93decb65609fcda0201
Paul Rossotti <rossotti.paul@gmail.com>
2016-06-06 17:00:25 -0700

    add filename-pattern to inbound-channel-adapter

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/app/gateway/FileFilter.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/app/gateway/FileFilter.java
@@ -1,6 +1,9 @@
 package com.rossotti.basketball.app.gateway;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 import org.springframework.integration.core.MessageSelector;
 import org.springframework.messaging.Message;
@@ -8,17 +11,22 @@ import org.springframework.messaging.Message;
 public class FileFilter implements MessageSelector {
 
 	public boolean accept(Message<?> message) {
-		if (message.getPayload() instanceof File) {
-			if (((File) message.getPayload()).getName().startsWith("service")) {
-//				File file = (File) message.getPayload();
-//				String payload = file.get;
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		return false;
-	}
+		Properties prop = new Properties();
 
+		if (message.getPayload() instanceof File) {
+			try {
+				FileInputStream fis = new FileInputStream((File) message.getPayload());
+				prop.load(fis);
+				String gameDate = prop.getProperty("game.date");
+				System.out.println("gameDate = " + gameDate);
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
