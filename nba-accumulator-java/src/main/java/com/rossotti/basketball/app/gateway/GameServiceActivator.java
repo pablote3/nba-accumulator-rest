ba21dca788c6180f7eb4f8db48bc419322efff61
Paul Rossotti <rossotti.paul@gmail.com>
2016-06-09 06:23:13 -0700

    remove wrapping arguments as Message

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/app/gateway/GameServiceActivator.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/app/gateway/GameServiceActivator.java
@@ -1,14 +1,10 @@
 package com.rossotti.basketball.app.gateway;
 
-import org.springframework.messaging.Message;
-
 public class GameServiceActivator {
-
-	public Message<ServiceProperties> printMessage(Message<ServiceProperties> properties) {
+	public ServiceProperties findGames(ServiceProperties properties) {
 		System.out.println("Processing order");
-		String gameDate = properties.getPayload().getGameDate();
+		String gameDate = properties.getGameDate();
 		System.out.println("GameDate:  " + gameDate);
 		return properties;
 	}
-
 }
