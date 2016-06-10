a36744095895e5a7dfeb5852fe39bd33bb3e0bd9
Paul Rossotti <rossotti.paul@gmail.com>
2016-06-10 05:58:16 -0700

    rename FindGameActivator

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/app/gateway/FindGameActivator.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/app/gateway/FindGameActivator.java
@@ -1,6 +1,6 @@
 package com.rossotti.basketball.app.gateway;
 
-public class GameServiceActivator {
+public class FindGameActivator {
 	public ServiceProperties findGames(ServiceProperties properties) {
 		System.out.println("Processing order");
 		String gameDate = properties.getGameDate();
