1e656783e1b6f0538a6ce0e23019149dca2e65b4
Paul Rossotti <rossotti.paul@gmail.com>
2016-06-16 05:58:54 -0700

    remove polling

--- nba-accumulator-java/src/test/java/com/rossotti/basketball/app/gateway/Startup.java
+++ nba-accumulator-java/src/test/java/com/rossotti/basketball/app/gateway/Startup.java
@@ -3,13 +3,11 @@ package com.rossotti.basketball.app.gateway;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Startup {
-
 	@SuppressWarnings({ "resource", "unused" })
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {
 					"classpath:si-config.xml",
 					"classpath:applicationContext.xml"
 		});
-		while (true) {}
 	}
 }
