631058121cd4faf1b608e56291e1753885e46038
Paul Rossotti <rossotti.paul@gmail.com>
2016-07-01 18:29:40 -0700

    update tests to match reduced client json

--- nba-accumulator-java/src/test/java/com/rossotti/basketball/client/dto/DTOMapperTest.java
+++ nba-accumulator-java/src/test/java/com/rossotti/basketball/client/dto/DTOMapperTest.java
@@ -31,8 +31,8 @@ public class DTOMapperTest {
 		GameDTO game = mapper.readValue(baseJson, GameDTO.class);
 		Assert.assertEquals("detroit-pistons", game.away_team.getTeam_id());
 		Assert.assertEquals(17, game.home_period_scores[1]);
-		Assert.assertEquals("Bojan Bogdanović", game.home_stats[6].getDisplay_name());
-		Assert.assertEquals(0f, (float)game.home_stats[6].getFree_throw_percentage(), 0.0f);
+		Assert.assertEquals("Bojan Bogdanović", game.home_stats[0].getDisplay_name());
+		Assert.assertEquals(0f, (float)game.home_stats[0].getFree_throw_percentage(), 0.0f);
 		Assert.assertEquals("Zarba", game.officials[0].getLast_name());
 		Assert.assertEquals("completed", game.event_information.getStatus());
 		Assert.assertTrue(game.away_totals.getThree_point_field_goals_attempted().equals((short)24));
