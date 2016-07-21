3ad31deaf41df075ff84406c39f56291d05e9d72
Paul Rossotti <rossotti.paul@gmail.com>
2016-07-20 17:21:23 -0700

    move object mapper to getStatsDTO()

--- nba-accumulator-java/src/main/java/com/rossotti/basketball/client/service/ClientService.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/client/service/ClientService.java
@@ -1,25 +1,35 @@
 package com.rossotti.basketball.client.service;
 
+import java.io.IOException;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
 import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rossotti.basketball.app.exception.PropertyException;
+import com.rossotti.basketball.app.provider.JsonProvider;
 import com.rossotti.basketball.app.service.PropertyService;
+import com.rossotti.basketball.client.dto.StatsDTO;
+import com.rossotti.basketball.client.dto.StatusCodeDTO;
 
 @Service
 
 public class ClientService {
 	@Autowired
 	private PropertyService propertyService;
-	
+
+	private static ObjectMapper mapper = JsonProvider.buildObjectMapper();
 	private final Logger logger = LoggerFactory.getLogger(ClientService.class);
 	private Client client;
 
@@ -34,13 +44,38 @@ public class ClientService {
 		}
 	};
 
-	public Client getClient() {
+	private Client getClient() {
 		client = ClientBuilder.newBuilder().build();
 		client.register(clientFilter);
 		logger.info('\n' + "Client service initialized");
 		return client;
 	}
-	public void setClient(Client client) {
-		this.client = client;
+
+	public StatsDTO getStatsDTO(String url, StatsDTO statsDTO) {
+		Response response = getClient().target(url).request().get();
+		if (response.getStatus() != 200) {
+			statsDTO.setStatusCode(StatusCodeDTO.NotFound);
+			response.readEntity(String.class);
+		}
+		else {
+			try {
+				statsDTO = mapper.readValue(response.readEntity(String.class), statsDTO.getClass());
+				statsDTO.setStatusCode(StatusCodeDTO.Found);
+			}
+			catch (JsonParseException jpe) {
+				statsDTO.setStatusCode(StatusCodeDTO.ClientException);
+				jpe.printStackTrace();
+			}
+			catch (JsonMappingException jme) {
+				statsDTO.setStatusCode(StatusCodeDTO.ClientException);
+				jme.printStackTrace();
+			}
+			catch (IOException ioe) {
+				statsDTO.setStatusCode(StatusCodeDTO.ClientException);
+				ioe.printStackTrace();
+			}
+		}
+		response.close();
+		return statsDTO;
 	}
 }
--- nba-accumulator-java/src/main/java/com/rossotti/basketball/client/service/RestClientService.java
+++ nba-accumulator-java/src/main/java/com/rossotti/basketball/client/service/RestClientService.java
@@ -1,16 +1,12 @@
 package com.rossotti.basketball.client.service;
 
-import java.io.IOException;
-
-import javax.ws.rs.core.Response;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rossotti.basketball.app.provider.JsonProvider;
+import com.rossotti.basketball.app.business.GameBusiness;
+import com.rossotti.basketball.app.exception.PropertyException;
 import com.rossotti.basketball.app.service.PropertyService;
 import com.rossotti.basketball.client.dto.GameDTO;
 import com.rossotti.basketball.client.dto.RosterDTO;
@@ -26,52 +22,52 @@ public class RestClientService {
 	@Autowired
 	private PropertyService propertyService;
 
-	private static ObjectMapper mapper = JsonProvider.buildObjectMapper();
+	private final Logger logger = LoggerFactory.getLogger(GameBusiness.class);
 
 	private StatsDTO retrieveStats(String url, StatsDTO statsDTO) {
-		Response response = clientService.getClient().target(url).request().get();
-		
-		if (response.getStatus() != 200) {
-			statsDTO.setStatusCode(StatusCodeDTO.NotFound);
-			response.readEntity(String.class);
-		} else {
-			try {
-				statsDTO = mapper.readValue(response.readEntity(String.class), statsDTO.getClass());
-				statsDTO.setStatusCode(StatusCodeDTO.Found);
-			} catch (JsonParseException jpe) {
-				statsDTO.setStatusCode(StatusCodeDTO.ClientException);
-				jpe.printStackTrace();
-			} catch (JsonMappingException jme) {
-				statsDTO.setStatusCode(StatusCodeDTO.ClientException);
-				jme.printStackTrace();
-			} catch (IOException ioe) {
-				statsDTO.setStatusCode(StatusCodeDTO.ClientException);
-				ioe.printStackTrace();
-			}
-		}
-		response.close();
-		return statsDTO;
+		return clientService.getStatsDTO(url, statsDTO);
 	}
 
 	public GameDTO retrieveBoxScore(String event) {
-		String url = propertyService.getProperty_Http("xmlstats.urlBoxScore");
-		String eventUrl = url + event + ".json";
 		GameDTO dto = new GameDTO();
-		return (GameDTO)retrieveStats(eventUrl, dto);
+		try {
+			String url = propertyService.getProperty_Http("xmlstats.urlBoxScore");
+			String eventUrl = url + event + ".json";
+			dto = (GameDTO)retrieveStats(eventUrl, dto);
+		}
+		catch (PropertyException pe) {
+			logger.info("property exception = " + pe);
+			dto.setStatusCode(StatusCodeDTO.ServerException);
+		}
+		return dto;
 	}
 
 	public RosterDTO retrieveRoster(String event) {
-		String url = propertyService.getProperty_Http("xmlstats.urlRoster");
-		String eventUrl = url + event + ".json";
 		RosterDTO dto = new RosterDTO();
-		return (RosterDTO)retrieveStats(eventUrl, dto);
+		try {
+			String url = propertyService.getProperty_Http("xmlstats.urlRoster");
+			String eventUrl = url + event + ".json";
+			dto = (RosterDTO)retrieveStats(eventUrl, dto);
+		}
+		catch (PropertyException pe) {
+			logger.info("property exception = " + pe);
+			dto.setStatusCode(StatusCodeDTO.ServerException);
+		}
+		return dto;
 	}
 
 	public StandingsDTO retrieveStandings(String event) {
-		String url = propertyService.getProperty_Http("xmlstats.urlStandings");
-		String eventUrl = url + event + ".json";
 		StandingsDTO dto = new StandingsDTO();
-		return (StandingsDTO)retrieveStats(eventUrl, dto);
+		try {
+			String url = propertyService.getProperty_Http("xmlstats.urlStandings");
+			String eventUrl = url + event + ".json";
+			dto = (StandingsDTO)retrieveStats(eventUrl, dto);
+		}
+		catch (PropertyException pe) {
+			logger.info("property exception = " + pe);
+			dto.setStatusCode(StatusCodeDTO.ServerException);
+		}
+		return dto;
 	}
 
 	public ClientService getClientService() {
