36b1754d3e55a637dc9308c2d6800bdd8bc64003
Paul Rossotti <rossotti.paul@gmail.com>
2016-07-22 18:49:47 -0700

    convert to mocked unit tests

--- nba-accumulator-java/src/test/java/com/rossotti/basketball/client/service/FileStatsServiceTest.java
+++ nba-accumulator-java/src/test/java/com/rossotti/basketball/client/service/FileStatsServiceTest.java
@@ -1,64 +1,150 @@
 package com.rossotti.basketball.client.service;
 
-import java.io.IOException;
-
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import com.rossotti.basketball.app.exception.PropertyException;
+import com.rossotti.basketball.app.service.PropertyService;
 import com.rossotti.basketball.client.dto.GameDTO;
 import com.rossotti.basketball.client.dto.RosterDTO;
 import com.rossotti.basketball.client.dto.StandingsDTO;
-import com.rossotti.basketball.client.service.FileClientService;
+import com.rossotti.basketball.client.dto.StatsDTO;
+import com.rossotti.basketball.client.dto.StatusCodeDTO;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+@RunWith(MockitoJUnitRunner.class)
 public class FileStatsServiceTest {
-	@Autowired
-	private FileClientService fileClientService;
+	@Mock
+	PropertyService propertyService;
+
+	@Mock
+	FileClientService fileClientService;
+
+	@InjectMocks
+	private FileStatsService fileStatsService;
 
 	@Test
-	public void retrieveBoxScore_Found() throws IOException {
-		String event = "20150415-utah-jazz-at-houston-rockets";
-		GameDTO game = fileClientService.retrieveBoxScore(event);
-		Assert.assertTrue(game.isFound());
-		Assert.assertEquals("Utah Jazz", game.away_team.getFull_name());
+	public void retrieveBoxScore_propertyException() {
+		when(propertyService.getProperty_Path(anyString()))
+			.thenThrow(new PropertyException("propertyName"));
+		GameDTO game = fileStatsService.retrieveBoxScore("20150415-utah-jazz-at-houston-rockets");
+		Assert.assertTrue(game.isServerException());
 	}
+
 	@Test
-	public void retrieveBoxScore_NotFound() throws IOException {
-		String event = "20150415-utah-jazz-at-houston-rocketeers";
-		GameDTO game = fileClientService.retrieveBoxScore(event);
+	public void retrieveBoxScore_notFound() {
+		when(propertyService.getProperty_Path(anyString()))
+			.thenReturn("/home/pablote/");
+		when(fileClientService.retrieveStats(anyString(), anyString(), (StatsDTO) anyObject()))
+			.thenReturn(createMockGameDTO(new GameDTO(), StatusCodeDTO.NotFound));
+		GameDTO game = fileStatsService.retrieveBoxScore("20150415-utah-jazz-at-houston-rockets");
 		Assert.assertTrue(game.isNotFound());
 	}
 
 	@Test
-	public void retrieveRoster_Found() throws IOException {
-		String event = "toronto-raptors";
-		RosterDTO roster = fileClientService.retrieveRoster(event);
-		Assert.assertTrue(roster.isFound());
-		Assert.assertEquals("Toronto Raptors", roster.team.getFull_name());
+	public void retrieveBoxScore_clientException() {
+		when(propertyService.getProperty_Path(anyString()))
+			.thenReturn("/home/pablote/");
+		when(fileClientService.retrieveStats(anyString(), anyString(), (StatsDTO) anyObject()))
+			.thenReturn(createMockGameDTO(new GameDTO(), StatusCodeDTO.ClientException));
+		GameDTO game = fileStatsService.retrieveBoxScore("20150415-utah-jazz-at-houston-rockets");
+		Assert.assertTrue(game.isClientException());
 	}
+
 	@Test
-	public void retrieveRoster_NotFound() throws IOException {
-		String event = "toronto-raptiers";
-		RosterDTO roster = fileClientService.retrieveRoster(event);
+	public void retrieveBoxScore_found() {
+		when(propertyService.getProperty_Path(anyString()))
+		.thenReturn("/home/pablote/");
+		when(fileClientService.retrieveStats(anyString(), anyString(), (StatsDTO) anyObject()))
+			.thenReturn(createMockGameDTO(new GameDTO(), StatusCodeDTO.Found));
+		GameDTO game = fileStatsService.retrieveBoxScore("20150415-utah-jazz-at-houston-rockets");
+		Assert.assertTrue(game.isFound());
+	}
+
+	@Test
+	public void retrieveRoster_propertyException() {
+		when(propertyService.getProperty_Path(anyString()))
+			.thenThrow(new PropertyException("propertyName"));
+		RosterDTO roster = fileStatsService.retrieveRoster("toronto-raptors");
+		Assert.assertTrue(roster.isServerException());
+	}
+
+	@Test
+	public void retrieveRoster_notFound() {
+		when(propertyService.getProperty_Path(anyString()))
+			.thenReturn("/home/pablote/");
+		when(fileClientService.retrieveStats(anyString(), anyString(), (StatsDTO) anyObject()))
+			.thenReturn(createMockGameDTO(new RosterDTO(), StatusCodeDTO.NotFound));
+		RosterDTO roster = fileStatsService.retrieveRoster("toronto-raptors");
 		Assert.assertTrue(roster.isNotFound());
 	}
 
 	@Test
-	public void retrieveStandings_Found() throws IOException {
-		String event = "20141028";
-		StandingsDTO standings = fileClientService.retrieveStandings(event);
-		Assert.assertTrue(standings.isFound());
-		Assert.assertEquals("philadelphia-76ers", standings.standing[0].getTeam_id());
+	public void retrieveRoster_clientException() {
+		when(propertyService.getProperty_Path(anyString()))
+			.thenReturn("/home/pablote/");
+		when(fileClientService.retrieveStats(anyString(), anyString(), (StatsDTO) anyObject()))
+			.thenReturn(createMockGameDTO(new RosterDTO(), StatusCodeDTO.ClientException));
+		RosterDTO roster = fileStatsService.retrieveRoster("toronto-raptors");
+		Assert.assertTrue(roster.isClientException());
 	}
+
 	@Test
-	public void retrieveStandings_NotFound() throws IOException {
-		String event = "20140236";
-		StandingsDTO standings = fileClientService.retrieveStandings(event);
+	public void retrieveRoster_found() {
+		when(propertyService.getProperty_Path(anyString()))
+		.thenReturn("/home/pablote/");
+		when(fileClientService.retrieveStats(anyString(), anyString(), (StatsDTO) anyObject()))
+			.thenReturn(createMockGameDTO(new RosterDTO(), StatusCodeDTO.Found));
+		RosterDTO roster = fileStatsService.retrieveRoster("toronto-raptors");
+		Assert.assertTrue(roster.isFound());
+	}
+
+	@Test
+	public void retrieveStandings_propertyException() {
+		when(propertyService.getProperty_Path(anyString()))
+			.thenThrow(new PropertyException("propertyName"));
+		StandingsDTO standings = fileStatsService.retrieveStandings("20141028");
+		Assert.assertTrue(standings.isServerException());
+	}
+
+	@Test
+	public void retrieveStandings_notFound() {
+		when(propertyService.getProperty_Path(anyString()))
+			.thenReturn("/home/pablote/");
+		when(fileClientService.retrieveStats(anyString(), anyString(), (StatsDTO) anyObject()))
+			.thenReturn(createMockGameDTO(new RosterDTO(), StatusCodeDTO.NotFound));
+		StandingsDTO standings = fileStatsService.retrieveStandings("20141028");
 		Assert.assertTrue(standings.isNotFound());
+	}
+
+	@Test
+	public void retrieveStandings_clientException() {
+		when(propertyService.getProperty_Path(anyString()))
+			.thenReturn("/home/pablote/");
+		when(fileClientService.retrieveStats(anyString(), anyString(), (StatsDTO) anyObject()))
+			.thenReturn(createMockGameDTO(new RosterDTO(), StatusCodeDTO.ClientException));
+		StandingsDTO standings = fileStatsService.retrieveStandings("20141028");
+		Assert.assertTrue(standings.isClientException());
+	}
+
+	@Test
+	public void retrieveStandings_found() {
+		when(propertyService.getProperty_Path(anyString()))
+		.thenReturn("/home/pablote/");
+		when(fileClientService.retrieveStats(anyString(), anyString(), (StatsDTO) anyObject()))
+			.thenReturn(createMockGameDTO(new RosterDTO(), StatusCodeDTO.Found));
+		StandingsDTO standings = fileStatsService.retrieveStandings("20141028");
+		Assert.assertTrue(standings.isFound());
+	}
+
+	private StatsDTO createMockGameDTO(StatsDTO stats, StatusCodeDTO statusCode) {
+		stats.setStatusCode(statusCode);
+		return stats;
 	}
 }
